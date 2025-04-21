e6c5181fba933316e5cac4a7588aae5a0c0f0527
mark7 <mark7967@kyungmin.ac.kr>
2025-04-21 16:04:40 +0900

    Add Signup.java : 회원가입 요청용 DTO(Signup) 클래스 추가

---
+++ src/main/java/com/example/vet_backend/dto/Signup.java
@@ -0,0 +1,16 @@
+package com.example.vet_backend.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Signup {
+
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+}
