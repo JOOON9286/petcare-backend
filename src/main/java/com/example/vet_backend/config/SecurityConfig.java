f80e4dfb7bcb268bac50b4c8e8c26fc235bee318
minaaa <tera9131@gmail.com>
2025-05-11 23:54:07 +0900

    시큐리티 추가

--- src/main/java/com/example/vet_backend/config/SecurityConfig.java
+++ src/main/java/com/example/vet_backend/config/SecurityConfig.java
@@ -1,30 +1,44 @@
 package com.example.vet_backend.config;
-
+import com.example.vet_backend.filter.JwtAuthenticationFilter;
+import com.example.vet_backend.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors() // ✅ CORS 허용
-                .and()
-                .csrf(csrf -> csrf.disable()) // ✅ CSRF 비활성화
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)  //csrf 람다 변경
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT는 세션 사용 안 함
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/signup", // ✅ 회원가입 허용
-                                "/login"    // ✅ 로그인 허용
-
-
+                                "/api/auth/register",
+                                "/api/auth/login",
+                                "/api/users/register",
+                                "/api/users/login"
                         ).permitAll()
-                        .anyRequest().authenticated() // ✅ 그 외는 인증 필요
-                );
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN") // ROLE_ADMIN만 접근
+                        .requestMatchers("/api/user/**").hasAnyRole("USER", "ADMIN") // 둘 다 가능
+                        .anyRequest().authenticated()
+
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
