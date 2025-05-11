a7588df3341b41afd87a0cee3ec3d1715281e3d8
minaaa <tera9131@gmail.com>
2025-05-11 23:54:34 +0900

    user entity role, refreshToken 추가

--- src/main/java/com/example/vet_backend/entity/User.java
+++ src/main/java/com/example/vet_backend/entity/User.java
@@ -22,7 +22,14 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(unique = true, nullable = false)
     private String name;
 
     private String phone;
+
+    @Column(length = 500)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private String role = "ROLE_USER";
 }
