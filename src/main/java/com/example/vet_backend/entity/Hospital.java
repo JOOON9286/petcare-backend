d510a03bf545fb2ebba4ed112b4911288f6aac84
minaaa <tera9131@gmail.com>
2025-05-27 02:04:18 +0900

    Add @Getter @Setter

--- src/main/java/com/example/vet_backend/entity/Hospital.java
+++ src/main/java/com/example/vet_backend/entity/Hospital.java
@@ -1,9 +1,13 @@
 package com.example.vet_backend.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "hospital")
 public class Hospital {
@@ -11,7 +15,6 @@ public class Hospital {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long hospitalId;
-
     private String name;
     private String address;
     private Float latitude;
