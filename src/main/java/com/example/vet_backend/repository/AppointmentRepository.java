87a0bc552664d5b3ade0be72e489ef01ca9c2244
minaaa <tera9131@gmail.com>
2025-05-27 11:24:42 +0900

    Add AppointmentRepository 추가

--- src/main/java/com/example/vet_backend/repository/AppointmentRepository.java
+++ src/main/java/com/example/vet_backend/repository/AppointmentRepository.java
@@ -4,6 +4,13 @@ import com.example.vet_backend.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> findByVet_VetId(Long vetId);
+
+    List<Appointment> findByUser_UserId(Long userId);
+
 }
